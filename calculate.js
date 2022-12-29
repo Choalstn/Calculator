4e0d65cf1aeb985533672cd55e6da283db6f3c21
Choalstn <choalstn@gmail.com>
2022-12-29 17:16:09 +0900

    일부 구조 수정

--- calculate.js
+++ calculate.js
@@ -1,27 +1,27 @@
 const display = document.querySelector("#display--number");
 const buttons = document.querySelector("#btns");
-const procedure = document.querySelector(".calproce")
+const procedure = document.querySelector(".calproce");
 let firstNum, operatorForAdvanced, previousKey, previousNum;
 
 function calculate(n1, operator, n2) {
-    let result;
+  let result;
 
-    n1 = Number(n1);
-    n2 = Number(n2);
+  n1 = Number(n1);
+  n2 = Number(n2);
 
-    if(operator === "+") {
-        result = n1 + n2;
-    } else if(operator === "-") {
-        result = n1 - n2;
-    } else if(operator === "x") {
-        result = n1 * n2;
-    } else if(operator === "/") {
-        result = n1 / n2;
-    } else if(operator === "%") {
-        result = n1 % n2;
-    }
+  if (operator === "+") {
+    result = n1 + n2;
+  } else if (operator === "-") {
+    result = n1 - n2;
+  } else if (operator === "x") {
+    result = n1 * n2;
+  } else if (operator === "/") {
+    result = n1 / n2;
+  } else if (operator === "%") {
+    result = n1 % n2;
+  }
 
-    return String(result)
+  return String(result);
 }
 
 buttons.addEventListener("click", function (event) {
@@ -29,46 +29,48 @@ buttons.addEventListener("click", function (event) {
   const action = target.classList[0]; //target의 클래스 중 첫 번째
   const buttonContent = target.textContent;
 
-
   if (target.matches("button")) {
     if (action === "number") {
-        if(display.textContent === "0" || previousKey === "oper" || previousKey === "calculate") {
-            display.textContent = buttonContent;
-        } else if(previousKey === "number") {
-            display.textContent += buttonContent;
-        }
+      if (
+        display.textContent === "0" ||
+        previousKey === "oper" ||
+        previousKey === "calculate"
+      ) {
+        display.textContent = buttonContent;
+      } else if (previousKey === "number") {
+        display.textContent += buttonContent;
+      }
 
-        if(previousKey === "calculate") {
-            procedure.classList.add("display");
-        }
+      if (previousKey === "calculate") {
+        procedure.classList.add("display");
+      }
 
-        previousKey = "number";
+      previousKey = "number";
     }
     if (action === "oper") {
-        firstNum = display.textContent;
-        operatorForAdvanced = buttonContent;
-        console.log(firstNum, operatorForAdvanced);
+      firstNum = display.textContent;
+      operatorForAdvanced = buttonContent;
+      console.log(firstNum, operatorForAdvanced);
 
-        previousKey = "oper";
+      previousKey = "oper";
     }
     if (action === "decimal") {
-        previousKey = "decimal";
+      previousKey = "decimal";
     }
     if (action === "clear") {
-        firstNum = undefined;
-        secondNum = undefined;
-        operatorForAdvanced = undefined;
-        display.textContent = "0";
-        procedure.classList.add("display");
-        previousKey = "clear";
+      firstNum = undefined;
+      secondNum = undefined;
+      operatorForAdvanced = undefined;
+      display.textContent = "0";
+      procedure.classList.add("display");
+      previousKey = "clear";
     }
     if (action === "calculate") {
-        const secondNum = display.textContent;
-        procedure.textContent = firstNum + operatorForAdvanced + secondNum;
-        procedure.classList.remove("display");
-        display.textContent = calculate(firstNum, operatorForAdvanced, secondNum);
-        previousKey = "calculate";
+      const secondNum = display.textContent;
+      procedure.textContent = firstNum + operatorForAdvanced + secondNum;
+      procedure.classList.remove("display");
+      display.textContent = calculate(firstNum, operatorForAdvanced, secondNum);
+      previousKey = "calculate";
     }
-
   }
 });
